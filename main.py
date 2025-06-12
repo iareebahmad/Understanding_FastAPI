b2491c05f8ee539f9473a6c64df7557e2fb711eb
iareebahmad <m.areeb.ahmad@gmail.com>
2025-06-12 17:57:56 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ books = [
     {'Title': 'Title Five', 'Author': 'Author Five', 'category': 'math'},
     {'Title': 'Title Six', 'Author': 'Author Six', 'category': 'math'}
 ]
+@app.get("/",summary="Index Page Loading", description="Returns the welcome message")
+def get_all_books():
+    return {'Message':'Hello Hi! Welcome to the API Application'}
 
-@app.get("/demo", summary="Get all books", description="Returns the full list of available books.")
+
+@app.get("/books", summary="Get all books", description="Returns the full list of available books.")
 def get_all_books():
     return books
 
-@app.get("/demo/{dynamic_param}", summary="Get book by dynamic param", description="Returns the value of the dynamic path parameter.")
-def get_book_by_param(dynamic_param: str):
+@app.get("/books/{dynamic_param}", summary="Get book by dynamic param", description="Returns the value of the dynamic path parameter.")
+def get_all_books(dynamic_param: str):
     return {'dynamic_param': dynamic_param}
+
+@app.get("/books/mybook",summary="Get my Fav Book", description="Returns my fav book")
+def get_all_books():
+    return {'Book Title':'My fav Book'}
 
